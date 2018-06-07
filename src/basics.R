c714208ecedecfe91e90a05b885a116be60953af
mklau <matthewklau@fas.harvard.edu>
2018-06-06 21:45:52 -0400

    updating

---
+++ src/basics.R
@@ -0,0 +1,9 @@
+# Basic R script
+# MK Lau
+# 15 April 2018
+
+dat <- read.csv("./data/data.csv")
+
+cor.test(dat[,"x"], dat[,"y"])
+
+plot(y ~ x, data = dat)
--- src/script.R
+++ src/script.R
@@ -1,7 +1,23 @@
 # Here's my script
 
-2 + 2
+# File I/O = read.* and write.*
+# Working directories and relative paths = getwd, setwd, “./../../” 
+# Objects and assignments = x <- 10 (=, ->, assign)
+x <- read.csv("./data/data.csv")
 
-x <- 2 + 2
-y <- 3 + 2
-tree.ht <- read.csv("./data/data.csv")
+# Mode = Scalars, vectors, matrices, arrays, lists, data.frames 
+mode(x)
+
+# Class = numeric, character, factor, logical, data.frame, etc.
+class(x)
+
+# Indexing = x[1], x[1,1], x[[1]],
+x[1,1]
+x[,1]
+x[1,]
+x[[1]]
+
+# Sorting = sort(x), order(x)
+sort(x[,1])
+x[order(x[,1]),]
+
